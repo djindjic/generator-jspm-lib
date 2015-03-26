100747d5d5e157ede199035297eea72997137549
Aleksandar Djindjic <djindjic@gmail.com>
2015-03-26 10:45:11 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -39,7 +39,7 @@ module.exports = yeoman.generators.Base.extend({
       this.copy('jshintrc', '.jshintrc');
       this.copy('sauce_labs_capabilities.js', 'sauce_labs_capabilities.js');
       this.copy('travis.yml', '.travis.yml');
-      this.copy('karma.conf.js', '.karma.conf.js');
+      this.copy('karma.conf.js', 'karma.conf.js');
       this.directory('lib', 'lib');
       this.directory('test', 'test');
 
