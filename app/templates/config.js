3b7e090a5a563a971da1173d03170bdbc30fda1f
Aleksandar Djindjic <djindjic@gmail.com>
2015-05-11 07:55:48 +0200

    fixes #4
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

---
+++ app/templates/config.js
@@ -0,0 +1,28 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel-core@5.2.17",
+    "babel-runtime": "npm:babel-runtime@5.2.17",
+    "core-js": "npm:core-js@0.9.7",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:core-js@0.9.7": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
